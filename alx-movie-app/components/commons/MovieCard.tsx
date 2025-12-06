5e6bce0024a7e5897e0bee792225733452b4a107
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-12-06 21:14:56 +0000

    Implement movie fetching API, enhance Loading and MovieCard components, and create Movies page with filtering and pagination functionality in alx-movie-app.

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
 
-const MovieCard = () => {
-  return <div>{/* MovieCard component */}</div>;
+const MovieCard: React.FC<MovieProps> = ({
+  title,
+  posterImage,
+  releaseYear,
+}) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image
+          className="h-[430px] w-full rounded-md hover:cursor-pointer object-cover"
+          src={posterImage || "/placeholder-movie.jpg"}
+          width={300}
+          height={430}
+          alt={title || "Movie poster"}
+          unoptimized
+        />
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
